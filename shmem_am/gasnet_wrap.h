bb1c850fa283dc11f67935724c73c4765e6cdd1f
Siddhartha Jana <siddharthajana24@gmail.com>
2016-02-16 15:04:34 -0600

    Added a call to barrier_all to ensure completion of handler registration across PEs

--- shmem_am/gasnet_wrap.h
+++ shmem_am/gasnet_wrap.h
@@ -1,8 +1,8 @@
 
-typedef void (*shmemx_am_handler)  (void *buf, size_t nbytes, int is_request)
-int shmemx_amt_maxrequest();
+typedef void (*shmemx_am_handler)  (void *buf, size_t nbytes, int req_pe);
+int shmemx_am_maxrequest();
 void shmemx_am_attach (int function_id, shmemx_am_handler function_handler);
 void shmemx_am_detach(int function_id);
-void shmemx_am_launch(int dest, void* source_addr, size_t nbytes, int is_request, int handler_id);
+void shmemx_am_launch(int dest, int handler_id, void* source_addr, size_t nbytes);
 void shmemx_am_quiet();
 
--- tests/shmem_am_1.c
+++ tests/shmem_am_1.c
@@ -2,64 +2,38 @@
 #include <stdio.h>
 #include <shmem.h>
 #include "gasnet_wrap.h"
+#include "gasnet.h"
 
-
-#define hidx_ping_shorthandler   201
-#define hidx_pong_shorthandler   202
+#define HANDLER_ID_1 420
 
 volatile int flag = 0;
-int rank_shmem, size_shmem;
+int pe_id;
 
-void ping_shorthandler(shmemx_token_t token, void* temp_addr, size_t nbytes, int is_request)
+void sample_req_handler(void* temp_addr, size_t nbytes, int req_pe)
 {
-        printf("PE%d: Inside handler. Received %dbytes\t isrequest=%d\n", rank_shmem, nbytes, is_request);
-	flag = *((int*) temp_addr);
-	printf("PE%d: Leaving handler\n", rank_shmem);
+  printf("PE%d: Inside request handler. Received data = %d from PE%d\n",
+          pe_id, *((int*)temp_addr), req_pe);
 }
 
 
 int main(int argc, char **argv)
 {
-  size_t segsz = SHMEMX_PAGESIZE;
-  size_t heapsz = SHMEMX_PAGESIZE;
   int peer;
   int source_addr[1]; 
 
-  shmemx_handlerentry_t htable[] = { 
-      { hidx_ping_shorthandler,  ping_shorthandler  } 
-  };  
-  
-  //GASNET_SAFE(gasnet_init(&argc, &argv));
-  shmem_init();
-  size_shmem = shmem_n_pes();
-  rank_shmem = shmem_my_pe();
+  gasnet_init(&argc, &argv);
+  start_pes(0);
+  pe_id = shmem_my_pe();
+  peer = (pe_id+1)%shmem_n_pes();
 
-  printf("shmem: rank=%d, size=%d\n",rank_shmem, size_shmem);
+  /* initializing data payload with one integer */
+  source_addr[0]=6*peer;
 
-  segsz = shmemx_getMaxLocalSegmentSize();
-   
-  shmemx_attach(htable, sizeof(htable)/sizeof(shmemx_handlerentry_t),
-			                              segsz, 1048576*2);
-
-  if (rank_shmem) peer = 0;
-  else peer = 1;
-
-  flag = 0;
-  shmem_barrier_all();
-  /* Send a 1-sided active message to peer */
-  source_addr[0]=-8;
-  shmemx_AMSend(peer, hidx_ping_shorthandler, source_addr, sizeof(int), SHMEM_AM_ONESIDED);
-  while(flag!=-8) {
-	shmemx_AMPoll();		
-  }
-
-  printf("%d: I received %d from %d\n", rank_shmem, flag, peer);
-  shmem_barrier_all();
-
-  if (!rank_shmem || (rank_shmem == size_shmem-1))
-    printf("Hello from node %d of %d\n", (int)rank_shmem, (int)size_shmem);
+  /* Collective operation: Implicit barrier on return from attach */
+  shmemx_am_attach(HANDLER_ID_1, &sample_req_handler);
+  shmemx_am_launch(peer, HANDLER_ID_1, source_addr, sizeof(int));
+  shmemx_am_quiet();		
 
   shmem_finalize();
 }
-
 
